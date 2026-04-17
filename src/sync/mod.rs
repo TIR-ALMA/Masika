d35a74ad7c69bda7f803569beeb13acc8efe1f3e
TIR-ALMA <visotskayoa@mail.ru>
2026-04-17 16:26:01 +0700

    Create mod.rs

---
+++ src/sync/mod.rs
@@ -0,0 +1,38 @@
+pub mod spinlock;
+
+pub use spinlock::Spinlock;
+
+pub use spinlock::Mutex;
+
+pub use spinlock::RwLock;
+
+pub use spinlock::Once;
+
+pub use spinlock::Semaphore;
+
+pub use spinlock::Barrier;
+
+pub use spinlock::Condvar;
+
+pub use spinlock::AtomicUsize;
+
+pub use spinlock::AtomicBool;
+
+pub use spinlock::AtomicPtr;
+
+pub use spinlock::AtomicI32;
+
+pub use spinlock::AtomicU64;
+
+pub use spinlock::AtomicI64;
+
+pub use spinlock::AtomicU32;
+
+pub use spinlock::AtomicI8;
+
+pub use spinlock::AtomicU8;
+
+pub use spinlock::AtomicI16;
+
+pub use spinlock::AtomicU16;
+
